3cfaaa1c6ea4ef4d1dc1e50bddc3130a47c44bfd
Yayun Liu <krashilili0915@gmail.com>
2018-06-03 00:21:22 -0500

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'hgyVsdgvTG3R4QOojz3srsjFj'
-consumer_secret='LGWp6ABUGImSkv40685FDXOr0R90NQpD6OolDpg8Aw3kVMRvK3' 
-access_token = '999073676334071808-Z5HTn8yhnefPWWhW4lRMeWrOBtwP1PK'
-access_token_secret = 'YklQLQRw1478sMbkzbP87Qu9417QC4X3jd50wEFC1OtRA'
